06331e003ea7ed237ca9faf2303a8ebdaede7cd2
faranbutt <faranbutt789@gmail.com>
2023-03-23 23:59:30 +0500

    Updated the mid section of Website

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -23,11 +23,11 @@ export default function Hero(){
         >
           <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
             <img src={'/cover.png'} alt='cover' className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative" />
-            <Link href={'#explore'} >
+            <a href={'#explore'} >
             <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
               <img src={'/stamp.png'} alt='stamp' className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain" /> 
             </div>
-            </Link>
+            </a>
         </motion.div>
       </motion.div>
     </section>
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,9 +1,14 @@
 'use client';
+import { motion } from "framer-motion";
+import styles from "@/styles";
+import { staggerContainer,fadeIn, planetVariants } from "@/utils/motion";
+import { StartSteps, TitleText, TypingText } from "@/components";
+import { startingFeatures } from '../constants';
+import Image from "next/image";
 
-const WhatsNew = () => (
-  <section>
-    What's new section
-  </section>
-);
+export default function WhatsNew(){
+  return(
+    <section></section>
+  );
+}
 
-export default WhatsNew;
